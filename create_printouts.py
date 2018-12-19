32a8e9e30fd0276c1ea616d0c581a5eabd40e704
Guido Maria Serra <zeph@fsfe.org>
2018-12-19 18:01:56 +0100

    renaming for ease

---
+++ create_printouts.py
@@ -0,0 +1,11 @@
+import pyqrcode
+content_a = []
+with open("chiave.armored") as f:
+    content_a = f.readlines()
+righe = len(content_a)
+content1 = ''.join(content_a[:righe//2])
+content2 = ''.join(content_a[righe//2:])
+qr1 = pyqrcode.create(content1, version=21, error='L')
+qr1.png("horn1.png", scale=6)
+qr2 = pyqrcode.create(content2, version=21, error='L')
+qr2.png("horn2.png", scale=6)
