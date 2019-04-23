2b048272b7878853a2a1176423591dc8c4f8cb45
Guido Serra <zeph@fsfe.org>
2019-04-23 14:41:51 +0200

    first generalization

--- create_printouts.py
+++ create_printouts.py
@@ -1,11 +1,13 @@
 import pyqrcode
-content_a = []
+content = ""
+c_limit = 512
+
 with open("chiave.armored") as f:
-    content_a = f.readlines()
-righe = len(content_a)
-content1 = ''.join(content_a[:righe//2])
-content2 = ''.join(content_a[righe//2:])
-qr1 = pyqrcode.create(content1, version=21, error='L')
-qr1.png("horn1.png", scale=6)
-qr2 = pyqrcode.create(content2, version=21, error='L')
-qr2.png("horn2.png", scale=6)
+    content = ''.join(f.readlines())
+
+c_count = len(content)/c_limit
+
+for i in xrange(0, c_count):
+    chunk = content[i*c_limit:c_limit]
+    qr = pyqrcode.create(chunk, version=21, error='L')
+    qr.png('chunk{0}.png'.format(i), scale=6)
